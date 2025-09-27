258a31e7f0b43285d2cda02861f9a0e1e0b41ee4
YeyoM <yeyomoreno2003@hotmail.com>
2025-09-27 17:29:24 -0600

    parcial02: Add account service

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
 
@@ -8,15 +8,22 @@ import { AccountService } from '../../core/services/account-service';
   templateUrl: './nav.html',
   styleUrl: './nav.css'
 })
-
 export class Nav {
   private accountService = inject(AccountService);
   protected creds: any = {};
+  protected loggedIn = signal(false);
 
   login(): void {
     this.accountService.login(this.creds).subscribe({
-      next: response => console.log(response),
+      next: response => {
+        console.log(response);
+        this.loggedIn.set(true);
+      },
       error: error => alert(error.message)
     });
+  }
+
+  logout(): void {
+    this.loggedIn.set(false);
   }
 }
