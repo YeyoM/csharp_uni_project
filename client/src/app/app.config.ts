90e818020acfb3e9acd6ad6b6987f79ee58a1fee
YeyoM <yeyomoreno2003@hotmail.com>
2025-10-25 13:13:57 -0600

    parcial02: Add app init

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,5 +1,7 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, inject, provideAppInitializer, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { InitService } from '../core/services/init-service';
+import { lastValueFrom } from 'rxjs';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
@@ -9,6 +11,22 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideHttpClient(),
+    provideAppInitializer(async () => {
+      const initService = inject(InitService);
+      return new Promise<void>((resolve) => {
+        setTimeout(async () => {
+          try {
+            return lastValueFrom(initService.init())
+          } finally {
+            const splash = document.getElementById("initial-splash");
+            if (splash) {
+              splash.remove();
+            }
+            resolve();
+          }
+        }, 500);
+      });
+    }),
     provideRouter(routes)
   ]
 };
