a0e2f52f8133041d1d6175e8301c710110298f28
YeyoM <yeyomoreno2003@hotmail.com>
2025-10-25 13:27:41 -0600

    parcial02: Add Http Interceptor

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -4,13 +4,14 @@ import { InitService } from '../core/services/init-service';
 import { lastValueFrom } from 'rxjs';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { errorInterceptor } from '../core/interceptors/error-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([errorInterceptor])),
     provideAppInitializer(async () => {
       const initService = inject(InitService);
       return new Promise<void>((resolve) => {
