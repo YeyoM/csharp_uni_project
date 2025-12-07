e31601ad7c8d6d732bd7ae99b6b96b60805cd1d1
YeyoM <yeyomoreno2003@hotmail.com>
2025-12-06 18:11:05 -0600

    Add member interface to spa

--- API/Extensions/AppUserExtension.cs
+++ API/Extensions/AppUserExtension.cs
@@ -1,10 +1,11 @@
+using System.Reflection;
 using API.DTOs;
 using API.Entities;
 using API.Interfaces;
 
-namespace API.Extensions;
+namespace API.Mappers;
 
-public static class AppUserExtensions
+public static class AppUserMapper
 {
     public static UserResponse ToDto(this AppUser user, ITokenService tokenService)
     {
@@ -13,6 +14,7 @@ public static class AppUserExtensions
             Id = user.Id,
             DisplayName = user.DisplayName,
             Email = user.Email,
+            Image = user.ImageUrl,
             Token = tokenService.CreateToken(user)
         };
     }
